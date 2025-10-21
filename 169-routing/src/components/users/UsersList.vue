af0e475470d1bbbc32e1491954cf8797399b39c7
phamtam215 <pvtam21599@gmail.com>
2025-10-21 23:16:42 +0700

    :+1: 179- Add param props to navigate link

--- 169-routing/src/components/users/UsersList.vue
+++ 169-routing/src/components/users/UsersList.vue
@@ -1,6 +1,12 @@
 <template>
+  <button @click="confirmInput">Confirm</button>
   <ul>
-    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      :name="user.fullName"
+      :role="user.role"
+    ></user-item>
   </ul>
 </template>
 
@@ -12,6 +18,14 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  methods: {
+    confirmInput() {
+      // do something
+      //this.$router.push('/teams');
+      // ✅ Chuyển từ route hiện tại sang route '/teams'
+      this.$router.push('/teams');
+    },
+  },
 };
 </script>
 
