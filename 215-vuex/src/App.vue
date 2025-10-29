08f9289db6ea01e5f2b10d1836b4a6e6ae7c5cae
Pham Van Tam <pham.van.tam@azoom.jp>
2025-10-29 13:22:36 +0700

    :+1: Add a simple store

--- 215-vuex/src/App.vue
+++ 215-vuex/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <h3>Counter: {{ counter }}</h3>
+    <button @click="addOne">Increment</button>
+  </base-container>
 </template>
 
 <script>
@@ -8,6 +11,16 @@ import BaseContainer from './components/BaseContainer.vue';
 export default {
   components: {
     BaseContainer,
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+  methods: {
+    addOne() {
+      this.$store.state.counter++;
+    },
   },
 };
 </script>
--- 215-vuex/src/main.js
+++ 215-vuex/src/main.js
@@ -1,7 +1,17 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state() {
+    return {
+      counter: 0, // Biến state tập trung, dùng chung cho toàn app
+    };
+  },
+});
 
 const app = createApp(App);
 
+app.use(store);
 app.mount('#app');
